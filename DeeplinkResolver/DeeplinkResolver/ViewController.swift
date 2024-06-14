c316928bcbe1ae2d45796dffd1f6f37aaed1ec9d
Yuri Cavalcanti <yuri.cavalcanti2013@gmail.com>
2024-06-14 15:05:07 -0300

    add project

---
+++ DeeplinkResolver/DeeplinkResolver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DeeplinkResolver
+//
+//  Created by Yuri Cavalcanti on 14/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
